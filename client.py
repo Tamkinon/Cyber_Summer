6ad59bdd8bc50fa5d31f5334e2b532656af0f245
Spider235 <122629888+Spider235@users.noreply.github.com>
2023-09-17 17:38:49 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,3 @@
-    my_socket = socket.socket()
-    my_socket.connect(("127.0.0.1", 5555))
-    print("Connected to the server.")
+my_socket = socket.socket()
+my_socket.connect(("127.0.0.1", 5555))
+print("Connected to the server.")
