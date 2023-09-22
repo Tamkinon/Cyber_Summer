eb3f058061b571d09b31504b748d59bc1ab11828
Tamkinon <tamirbendavid95@gmail.com>
2023-09-22 22:53:21 +0300

    removed redundant constant

--- chat/client.py
+++ chat/client.py
@@ -3,8 +3,6 @@ import tkinter as tk
 import select
 from tkinter import *
 
-LENGTH_FIELD_SIZE = 4
-
 my_socket = socket.socket()
 my_socket.connect(("127.0.0.1", 5555))
 print("Connected to the server.")
