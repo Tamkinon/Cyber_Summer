e4a0df65335db927ae8d7398598085bcecbadec4
Spider235 <orikinu@gmail.com>
2023-09-30 19:07:42 +0300

    updated - 12.6 - 5-7

--- chat/client.py
+++ chat/client.py
@@ -13,6 +13,9 @@ print("Connected to the server.")
 socket_messages = [my_socket]
 my_msg = []
 
+check_if_name_is_taken = True
+button_options_window = None
+
 
 def send_message(msg_entry):
     original_message = msg_entry.get("1.0", "end-1c")
@@ -34,12 +37,16 @@ def receive_messages(text_widget):
             message_length = my_socket.recv(4).decode()
             data = my_socket.recv(int(message_length)).decode()
             if client_name == "Server":
-                create_message_box(data, "top", "gray82", client_name)
+                splitted_message = data.split()
+                if splitted_message[-1] == name:
+                    root.destroy()
+                else:
+                    create_message_box(data, "top", "gray82", client_name)
             else:
                 create_message_box(data, "right", "LightCyan3", client_name)
 
 
-def create_message_box(message, side, colour, name):
+def create_message_box(message, side, colour, user_name):
     # bd (border width) - specifies the width of the border around the frame
     # relief - specifies the type of border or relief effect around the frame.
     # In this case, "solid" is used to create a solid border around the frame.
@@ -55,8 +62,8 @@ def create_message_box(message, side, colour, name):
     message_frame = tk.Frame(expanding_message_frame, bd=1, relief="solid", bg=colour)
     message_frame.pack(side=side, padx=(4.5, 8))
 
-    name_label = tk.Label(message_frame, text=name, font=("Arial", 14), justify="left", bg=colour, fg=random_colour(name))
-    name_label.pack(anchor="w")
+    name_label_button = tk.Button(message_frame, relief="solid", bd=0, anchor="w", text=check_if_manager(user_name), font=("Arial", 14), justify="left", bg=colour, fg=random_colour(user_name), command=lambda: name_button_options_window(user_name))
+    name_label_button.pack(fill="x", anchor="w")
 
     message_label = tk.Label(message_frame, text=message, justify="left", bg=colour, font=fnt, wraplength=450)
     message_label.pack(anchor="w")
@@ -71,8 +78,8 @@ def on_canvas_configure(event):
     # canvas.yview_moveto(1.0)
 
 
-def random_colour(name):
-    random.seed(name)
+def random_colour(c_name):
+    random.seed(c_name)
     color = random.randrange(0, 2 ** 16)
     hex_color = hex(color)
     colour_string = "#" + hex_color[2:4] + "00" + hex_color[-2:]
@@ -83,12 +90,55 @@ def update_time():
     return datetime.datetime.now().strftime("%H:%M")
 
 
-def on_mousewheel(event):
-    canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+def check_if_have_access(button, user_name):
+    my_socket.send((str(len(name)).zfill(2) + name + "6").encode())
+    s_response = my_socket.recv(1024).decode()
+    manager_list = eval(s_response)
+    if name not in manager_list or name == user_name:
+        button.configure(state=tk.DISABLED)
+    else:
+        button.configure(state=tk.ACTIVE)
 
 
-name = input("What is your name? ")
-name_colour = random_colour(name)
+def check_if_manager(user_name):
+    my_socket.send((str(len(user_name)).zfill(2) + user_name + "6").encode())
+    s_response = my_socket.recv(1024).decode()
+    manager_list = eval(s_response)
+    if user_name in manager_list:
+        user_name = '@' + user_name
+    return user_name
+
+
+def kick_button(user_name, window):
+    message = str(len(name)).zfill(2) + name + "3" + str(len(user_name)).zfill(4) + user_name
+    my_socket.send(message.encode())
+    window.destroy()
+
+
+def name_button_options_window(user_name):
+    global button_options_window
+    if not button_options_window:
+        button_options_window = tk.Toplevel()
+        button_options_window.title("Options Window")
+        button_options_window.resizable(width=False, height=False)
+        button_options_window.configure(bg="mintcream")
+        button_options_window.geometry("200x200")
+        button1 = tk.Button(button_options_window, text="Kick", command=lambda: kick_button(user_name, button_options_window))
+        button1.grid(row=0, column=0)
+        check_if_have_access(button1, user_name)
+        button_options_window.mainloop()
+
+
+while check_if_name_is_taken:
+    name = input("What is your name? ")
+    name_to_server = str(len(name)).zfill(2) + name + "0"
+    my_socket.send(name_to_server.encode())
+    server_response = my_socket.recv(1024).decode()
+    if server_response == 'NO':
+        print("The name is already taken / has '@' in it / Your name cannot be 'Server'.")
+    if server_response == 'OK':
+        check_if_name_is_taken = False
+        name_colour = random_colour(name)
 
 
 # Create the main GUI window
@@ -110,8 +160,6 @@ canvas.pack(side="left", fill="both", expand=True)
 # Create a scroll bar
 scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
 scrollbar.pack(side="right", fill="y")
-canvas.bind_all("<MouseWheel>", on_mousewheel)
-canvas.configure(yscrollcommand=scrollbar.set)
 
 # Configure the canvas to work with the scrollbar
 canvas.configure(yscrollcommand=scrollbar.set)
--- chat/server_multi_tcp.py
+++ chat/server_multi_tcp.py
@@ -11,6 +11,14 @@ def print_client_sockets(clients):
         print("\t", c.getpeername())
 
 
+def check_if_name_in_list(name):
+    if name not in name_list and name != 'Server' and name[0] != '@':
+        name_list.append(name)
+        current_socket.send("OK".encode())
+    else:
+        current_socket.send("NO".encode())
+
+
 print("Setting up server...")
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((SERVER_IP, SERVER_PORT))
@@ -19,9 +27,13 @@ print("Listening for clients...")
 
 client_sockets = []
 messages_to_send = []
+# managers stuff
+managers_list = ['Ori', 'Tamir', 'Inbar']
+name_list = []
 
 
 while True:
+    kicked_client_socket = ''
     rlist, wlist, xlist = select.select([server_socket] + client_sockets, client_sockets, [])
     for current_socket in rlist:
         if current_socket is server_socket:
@@ -30,27 +42,45 @@ while True:
             client_sockets.append(connection)
             print_client_sockets(client_sockets)
         else:
+            data = ''
+            message_length = ''
             name_length = current_socket.recv(2).decode()
             client_name = current_socket.recv(int(name_length)).decode()
             client_command = current_socket.recv(1).decode()
-            message_length = current_socket.recv(4).decode()
-            data = current_socket.recv(int(message_length)).decode()
+            if client_command == '0':
+                check_if_name_in_list(client_name)
+            if client_command == '1':
+                message_length = current_socket.recv(4).decode()
+                data = current_socket.recv(int(message_length)).decode()
+                message = name_length.zfill(2) + client_name + message_length.zfill(4) + data
+                messages_to_send.append((current_socket, client_sockets, message))
+            if client_command == '3':
+                message_length = current_socket.recv(4).decode()
+                data = current_socket.recv(int(message_length)).decode()
+                message = client_name + " kicked " + data
+                message = "6".zfill(2) + "Server" + str(len(message)).zfill(4) + message
+                kicked_client_socket = client_sockets[name_list.index(data)]
+                messages_to_send.append((kicked_client_socket, client_sockets, message))
+            if client_command == '6':
+                current_socket.send(str(managers_list).encode())
             if data == "quit":
                 print("Connection closed", current_socket.getpeername())
                 data = client_name + " has left the chat!"
                 message = "6".zfill(2) + "Server" + str(len(data)).zfill(4) + data
                 messages_to_send.append((current_socket, client_sockets, message))
-                #current_socket.recv(MAX_MSG_LENGTH).decode()
                 client_sockets.remove(current_socket)
                 current_socket.close()
                 print_client_sockets(client_sockets)
-            else:
-                message = name_length.zfill(2) + client_name + message_length.zfill(4) + data
-                messages_to_send.append((current_socket, client_sockets, message))
 
     for message in messages_to_send:
         for receiver_socket in client_sockets:
             sender_socket, client_sockets, data = message
+            if sender_socket == kicked_client_socket and sender_socket in wlist:
+                kicked_client_socket.send(data.encode())
+                if receiver_socket == kicked_client_socket:
+                    client_sockets.remove(kicked_client_socket)
+                    kicked_client_socket.close()
+                    print_client_sockets(client_sockets)
             if sender_socket in wlist and receiver_socket is not sender_socket:
                 print(data)
                 receiver_socket.send(data.encode())
